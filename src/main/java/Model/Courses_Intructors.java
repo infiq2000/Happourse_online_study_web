56454fcad3ef3d5407f1c46ef448e6475ac3af22
nethead <hleemin1905@gmail.com>
2022-03-22 16:15:38 +0700

    [NEW] Manage courses page for instructor

---
+++ src/main/java/Model/Courses_Intructors.java
@@ -0,0 +1,12 @@
+package Model;
+
+public class Courses_Intructors extends Courses {
+
+	public Courses_Intructors(int courses_id, String name, String skill, int price, String language, double star_rate,
+			String desciption, int ins_id, int cid) {
+		super(courses_id, name, skill, price, language, star_rate, desciption, ins_id, cid);
+		int totalSales;
+	}
+	
+	
+}
