d47e00187859b8d52e8343ca94487f92543fdbb2
nethead <hleemin1905@gmail.com>
2022-03-31 17:54:03 +0700

    [UPDATE] Show courses's mood

--- src/main/java/Model/ManagedCourses.java
+++ src/main/java/Model/ManagedCourses.java
@@ -5,6 +5,8 @@ import java.sql.Date;
 public class ManagedCourses extends Courses{
 	private String category;
 	private Date publishDate;
+	private String mood;
+	private String color;
 	public String getCategory() {
 		return category;
 	}
@@ -17,10 +19,25 @@ public class ManagedCourses extends Courses{
 	public void setPublishDate(Date publishDate) {
 		this.publishDate = publishDate;
 	}
+	public String getMood() {
+		return mood;
+	}
+	public void setMood(String mood) {
+		this.mood = mood;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
 	public ManagedCourses(int courses_id, String name, String skill, int price, String language, double star_rate,
-			String desciption, int ins_id, int cid, int countCourses, String category, Date publishDate) {
+			String desciption, int ins_id, int cid, int countCourses, String category, Date publishDate, String mood,
+			String color) {
 		super(courses_id, name, skill, price, language, star_rate, desciption, ins_id, cid, countCourses);
 		this.category = category;
 		this.publishDate = publishDate;
+		this.mood = mood;
+		this.color = color;
 	}
 }
