bc1d50a810303412d0f2ab63558ea8d41c08e4de
infiq2000 <dathtfx11133@funix.edu.vn>
2022-04-01 18:25:33 +0700

    add new java class: courseLesson.java

---
+++ src/main/java/Controller/CourseLesson.java
@@ -0,0 +1,101 @@
+package Controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import Dao.AccountUtil;
+import Dao.InstructorUtil;
+import Dao.LectureUtil;
+import Dao.UserUtil;
+import Model.Chapter;
+import Model.Courses;
+import Model.Instructor;
+
+/**
+ * Servlet implementation class CourseLesson
+ */
+@WebServlet("/CourseLesson")
+public class CourseLesson extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	@Resource(name="jdbc/Happourse")
+	private DataSource dataSource;
+	Dao.CourseUtil courseUtil; 
+	AccountUtil accUtil; 
+    UserUtil userUtil;
+    InstructorUtil insUtil;
+    LectureUtil lecUtil;
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		super.init();
+		accUtil = new AccountUtil(dataSource);
+		userUtil = new UserUtil(dataSource);
+		courseUtil = new Dao.CourseUtil(dataSource);
+		insUtil = new InstructorUtil(dataSource);
+		lecUtil = new LectureUtil(dataSource);
+	}
+    public CourseLesson() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		int course_id = Integer.parseInt(request.getParameter("course_id"));
+		Courses detailC=null;
+		try {
+			detailC = courseUtil.getCourseDetail(course_id);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		request.setAttribute("course_detail",detailC);	
+		
+		String cate="";
+		try {
+			cate = insUtil.getCate(detailC.getCid());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		request.setAttribute("cate", cate);
+		
+		Instructor ins_info=null;
+		try {
+			ins_info = insUtil.getIns_Info(detailC.getIns_id());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		request.setAttribute("ins_info", ins_info);
+		
+		List<Chapter> list_chapter=null;
+		try {
+			list_chapter = lecUtil.getChapterOfCourse(detailC.getCourses_id());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		request.setAttribute("list_chapter", list_chapter);
+		RequestDispatcher dispatcher;
+		dispatcher = request.getRequestDispatcher("/course_lession.jsp");					
+		dispatcher.forward(request, response);
+	}
+
+}
