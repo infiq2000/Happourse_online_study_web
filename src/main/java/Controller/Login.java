9ad82af67c31894907f1ce52fe06ff73522fd5f1
nguyenducanson <65609773+nguyenducanson@users.noreply.github.com>
2022-04-14 14:39:58 +0700

    update FE

--- src/main/java/Controller/Login.java
+++ src/main/java/Controller/Login.java
@@ -86,6 +86,7 @@ public class Login extends HttpServlet {
 					
 					int pagesNumber = couUtil.courseNumberPage(courses);
 					List<Courses> li = couUtil.getCoursesByPage(courses, 1);
+					
 					request.setAttribute("listCourses", li);
 					request.setAttribute("pagesNumber", pagesNumber);
 					
